41fc536574847b568c672262eea8082a0f8f0404
Shinozaki Robyn Natsuko <shinozakirobynnatsuko@Shinozakis-MacBook-Air.local>
2016-07-29 22:33:33 +0545

    add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
+
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -11,6 +13,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
